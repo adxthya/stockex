4aaad3af25b37d4717c80018c38193ff460060c5
Adithya K B <adithyakb93@gmail.com>
2024-04-14 11:52:35 +0530

    Add transition properties

--- app/api/fetchdata.ts
+++ app/api/fetchdata.ts
@@ -2,32 +2,28 @@
 import puppeteer from "puppeteer";
 
 export default async function fetchData(stock: string) {
-  // const url = `https://www.google.com/finance/quote/${stock}:NSE?hl=en`;
-  // const browser = await puppeteer.launch();
-  // const page = await browser.newPage();
-  // await page.goto(url);
-  // const priceElement = await page.$(".fxKbKc");
-  // const price = await (
-  //   await priceElement?.getProperty("textContent")
-  // )?.jsonValue();
-  // const varianceElement = await page.waitForSelector("span.ZYVHBb");
-  // const variance = await (
-  //   await varianceElement?.getProperty("textContent")
-  // )?.jsonValue();
-  // await page.goto(url + "&window=1Y");
-  // const yearReturnsElement = await page.waitForSelector("span.ZYVHBb");
-  // const yearReturns = await (
-  //   await yearReturnsElement?.getProperty("textContent")
-  // )?.jsonValue();
-  // await page.goto(url + "&window=1M");
-  // const monthReturnsElement = await page.waitForSelector("span.ZYVHBb");
-  // const monthReturns = await (
-  //   await monthReturnsElement?.getProperty("textContent")
-  // )?.jsonValue();
-  // browser.close();
-  const price = "20";
-  const variance = "30";
-  const yearReturns = "40";
-  const monthReturns = "50";
+  const url = `https://www.google.com/finance/quote/${stock}:NSE?hl=en`;
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  const priceElement = await page.$(".fxKbKc");
+  const price = await (
+    await priceElement?.getProperty("textContent")
+  )?.jsonValue();
+  const varianceElement = await page.waitForSelector("span.ZYVHBb");
+  const variance = await (
+    await varianceElement?.getProperty("textContent")
+  )?.jsonValue();
+  await page.goto(url + "&window=1Y");
+  const yearReturnsElement = await page.waitForSelector("span.ZYVHBb");
+  const yearReturns = await (
+    await yearReturnsElement?.getProperty("textContent")
+  )?.jsonValue();
+  await page.goto(url + "&window=1M");
+  const monthReturnsElement = await page.waitForSelector("span.ZYVHBb");
+  const monthReturns = await (
+    await monthReturnsElement?.getProperty("textContent")
+  )?.jsonValue();
+  browser.close();
   return { price, variance, yearReturns, monthReturns };
 }
